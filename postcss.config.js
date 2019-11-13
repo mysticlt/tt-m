f8de1b4f809fb305e9154322eb9f355d1e8b30dc
mysticlt <932721231@qq.com>
2019-11-13 21:10:11 +0800

    项目架构完成

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // 只是 px to rem 的配置
+    // 同时 是有一个插件 amfe-flexble 设置基准值
+    'postcss-pxtorem': {
+      // 换算rem的基准值 标准设备IPhone6 宽度375
+      // 设计稿是750px
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入设置基准值创建
+import 'amfe-flexible'
+// 导入Vant移动端组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 自己的样式
+import '@/styles/index.less'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
