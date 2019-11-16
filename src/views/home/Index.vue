d5f97e6e826dad759d6ce64d71a12a5a62b4d728
mysticlt <932721231@qq.com>
2019-11-16 17:55:28 +0800

    列表文章布局 完成

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -11,7 +11,41 @@
             :success-text="refreshSuccessText"
           >
             <van-list v-model="upLoading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-              <van-cell v-for="item in articles" :key="item">{{item}}</van-cell>
+              <!-- 文章内容 -->
+              <van-cell v-for="item in articles" :key="item">
+                <!-- 三张图 -->
+                <div class="article_item">
+                  <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+                  <div class="img_box">
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                  </div>
+                  <div class="info_box">
+                    <span>你像一阵风</span>
+                    <span>8评论</span>
+                    <span>10分钟前</span>
+                    <span class="close">
+                      <van-icon name="cross"></van-icon>
+                    </span>
+                  </div>
+                </div>
+                <!-- 一张图 -->
+                <div class="article_item">
+                  <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+                  <div class="img_box">
+                    <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                  </div>
+                  <div class="info_box">
+                    <span>你像一阵风</span>
+                    <span>8评论</span>
+                    <span>10分钟前</span>
+                    <span class="close">
+                      <van-icon name="cross"></van-icon>
+                    </span>
+                  </div>
+                </div>
+              </van-cell>
             </van-list>
           </van-pull-refresh>
         </div>
@@ -89,7 +123,6 @@ export default {
         }
       }, 1000)
     }
-
   }
 }
 </script>
@@ -147,6 +180,47 @@ export default {
     z-index: 1000;
     &::before {
       font-size: 20px;
+    }
+  }
+}
+// 文章列表样式
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
     }
   }
 }
